c536cd487da1eca3471f83b064c2be8a50755338
tranh1237 <tranhuyen123700@gmail.com>
2025-08-25 19:57:19 -0400

    Intitial commit

---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,31 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player;
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            _player = new Player(10, 10, 20, 0, 1);
+            Location location = new Location(1, "Home", "This is your house.");
+            
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
+        }
+    }
+}
