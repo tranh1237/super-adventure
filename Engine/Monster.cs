7d28069278a7c0dd2b1327b781170891c847d89f
tranh1237 <tranhuyen123700@gmail.com>
2025-08-28 00:08:58 -0400

    add remaining UI controls

--- Engine/Monster.cs
+++ Engine/Monster.cs
@@ -6,21 +6,23 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Monster: LivingCreature
+    public class Monster : LivingCreature
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int MaximumDamage { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
-
-        public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints) : base(currentHitPoints, maximumHitPoints)
+        public List<LootItem> LootTable { get; set; }
+        public Monster(int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints)
+            : base(currentHitPoints, maximumHitPoints)
         {
             ID = id;
             Name = name;
             MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
             RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
         }
     }
 }
